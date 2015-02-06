0ffb5f71cef49ebf8dfc683f2d27b638406ab613
Richard Dingwall <rdingwall@gmail.com>
2015-02-06 14:24:26 +0000

    Refined engine and tests to be more Go-centric

--- types.go
+++ types.go
@@ -4,22 +4,11 @@ import (
 	"fmt"
 )
 
+type Price uint16 // 0-65536 eg the price 123.45 = 12345
 type OrderID uint64
-
-// Price (0-65536 interpreted as divided by 100).
-// eg the range is 000.00-655.36
-// eg the price 123.45 = 12345
-// eg the price 23.45 = 2345
-// eg the price 23.4 = 2340
-type Price uint16
-
-// Order Size.
 type Size uint64
-
-// Side (Ask=1, Bid=0).
 type Side int
 
-// Limit Order.
 type Order struct {
 	symbol string
 	trader string
@@ -32,8 +21,8 @@ type Order struct {
 type Execution Order
 
 const (
-	Bid Side = 0
-	Ask      = 1
+	Bid Side = iota
+	Ask
 )
 
 func (o *Execution) String() string {
